9b488acd63fbf84e8c5fd550cc2de61b41a06d8d
ChanYoung <cyjoungg@gmail.com>
2022-03-27 17:33:07 +0900

    level1 greedy

---
+++ programmers/모의고사.py
@@ -0,0 +1,26 @@
+
+def solution(answers):
+    answer = []
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    a = b = c = 0
+    for i in range(len(answers)):
+        if answers[i] == pattern1[i % len(pattern1)]:
+            a += 1
+        if answers[i] == pattern2[i % len(pattern2)]:
+            b += 1
+        if answers[i] == pattern3[i % len(pattern3)]:
+            c += 1
+
+    result = [a,b,c]
+    max_num = max(result)
+    first = 0
+    
+    for i in range(len(result)):
+        if max_num == result[i]:
+            answer.append(i+1)
+    answer.sort()
+    return answer
+if __name__ == "__main__":
+    print (solution([2,1,2,3,2,4,2,5]))
