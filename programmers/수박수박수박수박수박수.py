38a97748a24c629ed1cd99281f93eca5d147a77f
JoungChanYoung <cysmile94@naver.com>
2022-05-24 06:00:04 +0900

    level1

---
+++ programmers/수박수박수박수박수박수.py
@@ -0,0 +1,13 @@
+#level1
+
+def solution(n):
+    answer = ''
+    for i in range(n):
+        if i % 2 == 0:
+            answer += '수'
+        else:
+            answer += '박'
+    return answer
+
+if __name__ == "__main__":
+    print(solution(5))
